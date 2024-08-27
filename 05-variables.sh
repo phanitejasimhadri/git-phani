b175d83718d11ba52d0cfc1b3dfa47a722403c11
phanitejasimhadri <phanitejasimhadri@gmail.com>
2024-08-27 09:26:08 -0400

    Variables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME
+
+echo "Entered username is: $USERNAME"
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "Entered password is: $PASSWORD"
